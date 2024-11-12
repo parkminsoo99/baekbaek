29ae018c249c52c7816c7e3469ec06cb30736a3b
박민수 <zzangorc99@naver.com>
2024-11-12 18:16:33 +0900

    [level 2] Title: 요격 시스템, Time: 417.31 ms, Memory: 187 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        Arrays.sort(targets, (a,b) -> Integer.compare(a[1],b[1]));
+        Deque<int[]> list = new ArrayDeque<>(Arrays.asList(targets));
+        while(!list.isEmpty()){
+            int[] poped1 = list.removeFirst();
+            int start1 = poped1[0];
+            int end1 = poped1[1];
+            while(!list.isEmpty()){
+                int[] poped2 = list.removeFirst();
+                int start2 = poped2[0];
+                int end2 = poped2[1];
+                if(end1 <= start2){
+                    list.addFirst(new int[] {start2, end2});
+                    break;
+                }
+            }
+            
+            answer +=1;   
+        }  
+        return answer;
+    }
+}
